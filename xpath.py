17aee9e14288afea4ec1694bd7933d1d0676010b
Keith Hall <kingkeith+github@gmail.com>
2015-09-07 14:06:48 +0300

    calculate element index when building xpath
    show in status bar when xpath is being calculated, because can be slow towards the end of large files
    plus use correct line ending when copying multiple xpaths to clipboard

--- xpath.py
+++ xpath.py
@@ -1,10 +1,11 @@
 import sublime
 import sublime_plugin
-import re
-
+import os
 
 def buildPath(view, selection):
     path = ['']
+    levelCounters = [{}]
+    firstIndexInXPath = 1
     
     tagRegions = view.find_by_selector('entity.name.tag.')
     selfEndingTag = False
@@ -17,19 +18,25 @@ def buildPath(view, selection):
         
         if selfEndingTag:
             path.pop()
+            levelCounters.pop()
         if firstChar == '<':
-            # check last char before end tag...
+            # check last char before end of tag...
             tagScope = view.extract_scope(region.end())
             selfEndingTag = view.substr(tagScope)[-2] == '/'
             
-            path.append(tagName)
+            levelCounters[len(levelCounters) - 1][tagName] = levelCounters[len(levelCounters) - 1].setdefault(tagName, firstIndexInXPath - 1) + 1
+            level = levelCounters[len(levelCounters) - 1].get(tagName)
+            path.append(tagName + '[' + str(level) + ']')
+            levelCounters.append({})
         elif firstChar == '/':
             if selection.end() > region.end():
                 path.pop()
+                levelCounters.pop()
             selfEndingTag = False
     
     if selfEndingTag and tagScope.end() <= selection.begin():
-        path.pop();
+        path.pop()
+        levelCounters.pop() # technically not necessary because unused, but here for correctness
     return path
 
 def isSGML(view):
@@ -43,9 +50,10 @@ def updateStatusIfSGML(view):
         updateStatus(view)
 
 def updateStatus(view):
+    view.set_status('xpath', 'XPath being calculated...')
     path = buildPath(view, view.sel()[0])
     response = '/'.join(path)
-    view.set_status('xpath', response)
+    view.set_status('xpath', 'XPath: ' + response)
 
 class XpathCommand(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -58,8 +66,9 @@ class XpathCommand(sublime_plugin.TextCommand):
                 path = buildPath(view, selection)
                 response += '/'.join(path)
                 if s != len(selections) - 1:
-                    response += '\n'
+                    response += os.linesep
             sublime.set_clipboard(response)
+            sublime.status_message('xpath(s) copied to clipboard')
         else:
             sublime.status_message('xpath not copied to clipboard - ensure syntax is set to xml or html')
 
